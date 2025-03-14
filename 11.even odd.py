70b8e57c08ac4a4b75cb7bd51825781efb16d4c0
ankyagg <ixaaniketwalanj@gmail.com>
2025-03-14 13:33:37 +0530

    Add files via upload

---
+++ 11.even odd.py
@@ -0,0 +1,10 @@
+#Develop a Python program that takes a numerical input and identifies
+#whether it is even or odd, utilizing conditional statements and loops.
+
+number = int(input("Enter the no. to check if even or odd : "))
+
+if (number%2==0):
+    print("It is a even no.")
+
+else:
+    print("It is a odd no.")
